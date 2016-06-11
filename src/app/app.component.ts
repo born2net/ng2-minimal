47c79b7405c5f747b96abc8a6d8fd213c6e29039
root <born2net@gmail.com>
2016-06-11 08:20:11 -0700

    rc.1 router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import {HeroService} from "./heroes/hero.service";
         HeroService
     ],
     directives: [ROUTER_DIRECTIVES],
-    template: `<h1>test AAA</h1>
+    template: `<h1>New ng2 Router</h1>
                 <nav>
                   <a [routerLink]="['/crisis-center']">Crisis Center</a>
                   <a [routerLink]="['/heroes']">Heroes</a>
@@ -24,29 +24,4 @@ export class AppComponent {
     constructor() {
     }
 }
-// @Component({
-//     selector: 'my-app',
-//     template: `
-//     <h1 class="title">Component Router</h1>
-//     <nav>
-//       <a [routerLink]="['/crisis-center']">Crisis Center</a>
-//       <a [routerLink]="['/heroes']">Heroes</a>
-//     </nav>
-//     <router-outlet></router-outlet>
-//   `,
-//     providers: [
-//         DialogService,
-//         HeroService
-//     ],
-//     directives: [ROUTER_DIRECTIVES]
-// })
-// export class AppComponent {
-// }
-//
-// Dynamic loading (old router but coming to new router)
-// {
-//   path: '/customers/:id/...',
-//   name: 'Customer',
-//   loader: () => window['System'].import('app/+customer')
-//                 .then((module: any) => module.CustomerComponent)
-// }
+
