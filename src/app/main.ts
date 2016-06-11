61ddf1f2072fbd863fb4915602c6fed7f8af9d00
root <born2net@gmail.com>
2016-06-11 11:29:49 -0700

    remove dup

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,12 @@
-import { bootstrap }            from '@angular/platform-browser-dynamic';
-import { AppComponent }         from './app.component';
-import { APP_ROUTER_PROVIDERS } from './app.routes';
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {AppComponent} from "./app.component";
+import {APP_ROUTER_PROVIDERS} from "./app.routes";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/do";
 
 // Extend Observable throughout the app
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
-
-import { AppComponent } from './app.component';
 
 bootstrap(AppComponent, [
     APP_ROUTER_PROVIDERS
